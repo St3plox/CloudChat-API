43ad26264e4858ba81d230cb9795bf7729460484
JegorSt3p <st3pegor@gmail.com>
2023-04-16 13:08:57 +0700

    added FileNotFoundException.java and FileExceptionHandler

--- src/main/java/ru/tvey/cloudserverapp/exception/file/FileExceptionHandler.java
+++ src/main/java/ru/tvey/cloudserverapp/exception/file/FileExceptionHandler.java
@@ -1,7 +1,6 @@
 package ru.tvey.cloudserverapp.exception.file;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
