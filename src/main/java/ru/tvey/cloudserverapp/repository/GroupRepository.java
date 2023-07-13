a88f4222b969fff6a40af32b7c6d7c0f07159c70
St3plox <st3pegor@gmail.com>
2023-07-13 11:52:10 +0600

    fixed GroupRepository

--- src/main/java/ru/tvey/cloudserverapp/repository/GroupRepository.java
+++ src/main/java/ru/tvey/cloudserverapp/repository/GroupRepository.java
@@ -1,6 +1,7 @@
 package ru.tvey.cloudserverapp.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import ru.tvey.cloudserverapp.entity.messaging.Group;
 
-public interface GroupRepository extends CrudRepository {
+public interface GroupRepository extends CrudRepository<Group, Long>{
 }
