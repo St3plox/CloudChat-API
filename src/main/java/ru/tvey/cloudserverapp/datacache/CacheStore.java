28ee6b2cf65c287051f6acc835033b2bc8eb7e26
St3plox <st3pegor@gmail.com>
2023-07-13 12:36:19 +0600

    added guava cache dependency and CacheStore.java

---
+++ src/main/java/ru/tvey/cloudserverapp/datacache/CacheStore.java
@@ -0,0 +1,33 @@
+package ru.tvey.cloudserverapp.datacache;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+public class CacheStore <T>{
+    private Cache<String, T> cache;
+
+    //Constructor to build Cache Store
+    public CacheStore(int expiryDuration, TimeUnit timeUnit) {
+        cache = CacheBuilder.newBuilder()
+                .expireAfterWrite(expiryDuration, timeUnit)
+                .concurrencyLevel(Runtime.getRuntime().availableProcessors())
+                .build();
+    }
+
+    //Method to fetch previously stored record using record key
+    public T get(String key) {
+        return cache.getIfPresent(key);
+    }
+
+    //Method to put a new record in Cache Store with record key
+    public void add(String key, T value) {
+        if(key != null && value != null) {
+            cache.put(key, value);
+            System.out.println("Record stored in "
+                    + value.getClass().getSimpleName()
+                    + " Cache with Key = " + key);
+        }
+    }
+}
