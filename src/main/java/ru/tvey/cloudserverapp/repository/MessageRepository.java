ac229e35b75e2244b2699f18312ca1406012bb32
St3plox <st3pegor@gmail.com>
2023-07-21 21:14:51 +0600

    MessageRepository added findLastNMessagesSortedByLocalDateAndGroup and findAllByGroupIdOrderByDateDesc methods

--- src/main/java/ru/tvey/cloudserverapp/repository/MessageRepository.java
+++ src/main/java/ru/tvey/cloudserverapp/repository/MessageRepository.java
@@ -1,7 +1,17 @@
 package ru.tvey.cloudserverapp.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.tvey.cloudserverapp.entity.messaging.Group;
 import ru.tvey.cloudserverapp.entity.messaging.Message;
 
-public interface MessageRepository extends CrudRepository<Message, Long> {
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+    default List<Message> findLastNMessagesSortedByLocalDateAndGroup(int n, Group groupId) {
+        PageRequest pageRequest = PageRequest.of(0, n, Sort.by(Sort.Direction.DESC, "sent_date", "group_id"));
+        return findAllByGroupIdOrderByDateDesc(groupId, pageRequest);
+    }
+    List<Message> findAllByGroupIdOrderByDateDesc(Group groupId, PageRequest pageRequest);
 }
